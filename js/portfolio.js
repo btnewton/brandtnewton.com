58960d91ac6bf2cda763e46f56da2128c10cb111
Brandt Newton <btnewton@uvm.edu>
2015-04-01 14:07:13 -0400

    async img loading

--- js/portfolio.js
+++ js/portfolio.js
@@ -8,18 +8,6 @@ $( document ).ready(function() {
 	content.hide();
 	hover_box.css("opacity", "0");
 
-	// hitbox.hover(
-	// 	function() {
-	// 		$( this ).find(content).stop(true).fadeIn();
-	// 		$( this ).find(".portfolio-icon").stop(true).animate({ opacity: "1"});
-	// 		$( this ).find(".portfolio-hover").stop(true).animate({ height: "100%", boxShadow: "0 -5px 5px rgba(0,0,0,0.5)" });
-	// 	},
-	// 	function() {
-	// 		$( this ).find(content).stop(true).fadeOut();
-	// 		$(this).find(".portfolio-hover").stop(true).animate({ height: "0" });
-	// 		$(this ).find(".portfolio-icon").stop(true).animate({ opacity: "1"});
-	// 	}
-	// );
 	hitbox.hover(
 		function() {
 			$( this ).find(content).stop(true).fadeIn();
