df5fe0aa3d417fedebab8c5140100633e3d0eb9e
unknown <bnewton149@gmail.com>
2015-08-21 17:34:10 -0400

    Fixing css that only looked good at desktop screen size

--- js/loadImages.js
+++ js/loadImages.js
@@ -2,6 +2,6 @@ $(window).load(function() {
 	$('span[img-path]').prepend(function(index){
 		var path = $(this).attr('img-path')
 		var imgClass = $(this).attr('img-class')
-		return '<img alt="" src="http://www.brandtnewton.com/img/' + path + '" class="' + imgClass + '"">'
+		return '<img alt="" src="img/' + path + '" class="' + imgClass + '"">'
 	})
 })
